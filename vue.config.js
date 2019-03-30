e9d39e3e73af4f15f2387ae3361752377d009ce5
rain <534123074@qq.com>
2019-03-30 10:49:00 +0800

    -修改打包后文件名

--- vue.config.js
+++ vue.config.js
@@ -1,7 +1,7 @@
 module.exports = {
   publicPath: './',
   //输出文件目录
-  outputDir: 'mcdonalds',
+  outputDir: 'dist',
   // eslint-loader 是否在保存的时候检查
   lintOnSave: true,
   //放置生成的静态资源 (js、css、img、fonts) 的 (相对于 outputDir 的) 目录。
